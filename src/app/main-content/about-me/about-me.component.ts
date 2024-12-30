862aff1b23d8281b42230aa04754dc0f456a9c36
Nicolas Voges <nicolas.voges@hotmail.com>
2024-12-30 04:25:07 +0100

    work on about me section and adjust some styles

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+  public aboutMeTexts: {
+    name: string;
+    text: string;
+  }[] = [{
+    name: 'location',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum quisquam iusto accusamus dolores tenetur quis corrupti, simil'
+  },
+  {
+    name: 'mind',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum quisquam iusto accusamus dolores tenetur quis corrupti, simil'
+  },
+  {
+    name: 'hook',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum quisquam iusto accusamus dolores tenetur quis corrupti, simil'
+  }];
 }
--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
-
+  public texts: string[] = [
+    'Open to work',
+    'Also available for remote work',
+    'Frontend Developer',
+    'Based in Wolfenbüttel',
+    'Aspiring Full Stack Developer'
+  ];
 }
