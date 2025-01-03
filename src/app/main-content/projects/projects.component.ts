f4a55b1375b568659c3a36c917df372563a112a4
Nicolas Voges <nicolas.voges@hotmail.com>
2025-01-04 00:30:14 +0100

    work on projects section

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  public projects: {
+    name: string;
+    skills: string[];
+  }[] = [{
+    name: 'Join',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Firabase']
+  },
+  {
+    name: 'El Pollo Loco',
+    skills: ['HTML', 'CSS', 'JavaScript']
+  },
+  {
+    name: 'DA Bubble',
+    skills: ['Angular', 'Firebase', 'TypeScript']
+  }];
 }
