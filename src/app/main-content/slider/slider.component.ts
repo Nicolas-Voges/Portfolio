5bfae43c3633d620d6faa7dfe5e048871864a6ed
Nicolas Voges <nicolas.voges@hotmail.com>
2025-01-08 23:25:19 +0100

    work on slider section

--- src/app/main-content/slider/slider.component.ts
+++ src/app/main-content/slider/slider.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
 export class SliderComponent {
-
+  public activIndex: number = 1;
+  public slides: {
+    text: string;
+    link?: string;
+  }[] = [
+      {
+        text: 'calc',
+        link: '',
+      },
+      {
+        text: 'snake',
+        link: '',
+      },
+      {
+        text: 'poke',
+        link: '',
+      }
+    ];
 }
