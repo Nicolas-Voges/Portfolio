15de7b154374966e4a27db49e946ef6fa2b9d5b3
Nicolas Voges <nicolas.voges@hotmail.com>
2025-01-10 23:00:02 +0100

    work on slider section and responsiveness

--- src/app/main-content/slider/slider.component.ts
+++ src/app/main-content/slider/slider.component.ts
@@ -27,4 +27,42 @@ export class SliderComponent {
         link: '',
       }
     ];
+    public animateLeftClasses: string[] = ['move-out-from-left', 'move-left-from-center', 'move-left-from-right'];
+    public animateRightClasses: string[] = ['move-right-from-left', 'move-right-from-center', 'move-out-from-right'];
+    public animateLeft = true;
+    public animateRight = false;
+
+  decreaseActiveIndex() {
+    this.activIndex--
+    if (this.activIndex < 0) {
+      this.activIndex = this.slides.length - 1;
+    }
+    this.animateLeft = true;
+    console.log(this.animateLeft);    
+    let id = setTimeout( () => {
+      let obj: {
+        text: string;
+        link?: string;
+      } | undefined = this.slides.pop();
+      if (obj != undefined) {
+        this.slides.unshift(obj);
+      }
+    this.animateLeft = false;
+    clearTimeout(id);
+    }, 1000);
+  }
+
+  increaseActiveIndex() {
+    this.activIndex++
+    if (this.activIndex >= this.slides.length) {
+      this.activIndex = 0;
+    }
+    let obj: {
+      text: string;
+      link?: string;
+    } | undefined = this.slides.shift();
+    if (obj != undefined) {
+      this.slides.push(obj);
+    }
+  }
 }
