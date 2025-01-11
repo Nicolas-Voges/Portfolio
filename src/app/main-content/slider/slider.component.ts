523bb46b9d578d99c394b8af7d84a958c633e292
Nicolas Voges <nicolas.voges@hotmail.com>
2025-01-11 02:22:03 +0100

    Work on readability and expandability of slider section

--- src/app/main-content/slider/slider.component.ts
+++ src/app/main-content/slider/slider.component.ts
@@ -27,50 +27,86 @@ export class SliderComponent {
         link: '',
       }
     ];
-    
+
   public animateLeft = false;
   public animateRight = false;
 
-  increaseActiveIndex() {
-    if (!this.animateLeft) {
+  getSlidesIndex(): number {
+    if (this.slides.length <= 3) {
+      return 0;
+    } else {
+      return 3;
+    }
+  }
+
+  leftAnimation() {
+    if (!this.animateLeft && !this.animateRight) {
       this.animateLeft = true;
-      this.activIndex++
-      if (this.activIndex >= this.slides.length) {
-        this.activIndex = 0;
+      this.increaseActiveIndex();
+      this.turnSlidesLeft();
+    }
+  }
+
+  increaseActiveIndex() {
+    this.activIndex++
+    if (this.activIndex >= this.slides.length) {
+      this.activIndex = 0;
+    }
+  }
+
+  turnSlidesLeft() {
+    let id = setTimeout(() => {
+      let obj: {
+        text: string;
+        link?: string;
+      } | undefined = this.slides.shift();
+      if (obj != undefined) {
+        this.slides.push(obj);
       }
-      console.log(this.animateLeft);
-      let id = setTimeout(() => {
-        let obj: {
-          text: string;
-          link?: string;
-        } | undefined = this.slides.shift(); //pop
-        if (obj != undefined) {
-          this.slides.push(obj);  //unshift
-        }
-        this.animateLeft = false;
-        clearTimeout(id);
-      }, 1000);
+      this.animateLeft = false;
+      clearTimeout(id);
+    }, 1000);
+  }
+
+  rightAnimation() {
+    if (!this.animateLeft && !this.animateRight) {
+      this.animateRight = true;
+      this.decreaseActiveIndex();
+      this.turnSlidesRight();
     }
   }
 
   decreaseActiveIndex() {
-    if (!this.animateRight) {
-      this.animateRight = true;
-      this.activIndex--
-      if (this.activIndex < 0) {
-        this.activIndex = this.slides.length - 1;
+    this.activIndex--
+    if (this.activIndex < 0) {
+      this.activIndex = this.slides.length - 1;
+    }
+  }
+
+  turnSlidesRight() {
+    let id = setTimeout(() => {
+      let obj: {
+        text: string;
+        link?: string;
+      } | undefined = this.slides.pop();
+      if (obj != undefined) {
+        this.slides.unshift(obj);
       }
-      let id = setTimeout(() => {
-        let obj: {
-          text: string;
-          link?: string;
-        } | undefined = this.slides.pop();
-        if (obj != undefined) {
-          this.slides.unshift(obj);
-        }
-        this.animateRight = false;
-        clearTimeout(id);
-      }, 1000);
+      this.animateRight = false;
+      clearTimeout(id);
+    }, 1000);
+  }
+
+  chooseAction(i: number) {
+    switch (i >= 0 && i <= 2) {
+      case i === 0:
+        this.rightAnimation();
+        break;
+      case i === 2:
+        this.leftAnimation();
+        break;
+      default:
+        break;
     }
   }
 }
