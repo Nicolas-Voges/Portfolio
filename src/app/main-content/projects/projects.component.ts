e2e06c3623af7062be49f59b0792445e4238bcf3
Nicolas Voges <nicolas.voges@hotmail.com>
2025-01-16 01:39:41 +0100

    work on project cards

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -12,16 +12,36 @@ export class ProjectsComponent {
   public projects: {
     name: string;
     skills: string[];
+    number: number;
+    description: string;
+    displayCard: boolean;
   }[] = [{
     name: 'Join',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Firabase']
+    skills: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
+    number: 1,
+    description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories',
+    displayCard: false
   },
   {
     name: 'El Pollo Loco',
-    skills: ['HTML', 'CSS', 'JavaScript']
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    number: 2,
+    description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+    displayCard: false
   },
   {
     name: 'DA Bubble',
-    skills: ['Angular', 'Firebase', 'TypeScript']
+    skills: ['Angular', 'Firebase', 'TypeScript'],
+    number: 3,
+    description: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+    displayCard: false
   }];
+
+  openProjectCard(i: number) {
+    this.projects[i].displayCard = true;
+  }
+
+  closeProjectCard(i: number) {
+    this.projects[i].displayCard = false;
+  }
 }
